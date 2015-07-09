eeb31ad622391f066f672595338b7f8f5bc70867
lucarin91 <lucarin91@gmail.com>
2015-07-09 20:59:59 +0200

    update database and parseCOmmand

--- test/dbdata.js
+++ test/dbdata.js
@@ -14,18 +14,6 @@ var insertTestComWords = function(){
 
 };
 
-var changeAdmin = function(){
-  db.Users.update({admin:true},{where:{username:'cipposo'}}).then(function(data){
-    console.log('done!');
-    console.log(data);
-  });
-
-  db.Users.update({admin:true},{where:{username:'CampariGin'}}).then(function(data){
-    console.log('done!');
-    console.log(data);
-  });
-}
-
 module.exports = function(){
   insertTestUsers();
   insertTestComWords();
